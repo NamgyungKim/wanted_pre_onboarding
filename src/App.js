d2261cfcf494d3b25060a360d23f7d0ec63fc8f3
NamgyungKim <namgyung.kim@gmail.com>
2022-02-06 01:48:10 +0900

    ✨ Feat: Modal 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Modal from "./component/Modal";
 import Toggle from "./component/Toggle";
 
 const App = () => {
   return (
     <div>
       <Toggle />
+      <Modal />
     </div>
   );
 };
---
+++ src/component/Modal.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Box } from "../styles/GlobalStyles";
+
+const Modal = () => {
+  // 모달창 보여주기 여부
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <Box>
+      <h1>Modal</h1>
+      <ModalBox>
+        {/* 모달버튼 */}
+        <ModalBtn type="button" onClick={() => setShowModal(!showModal)}>
+          Open Modal
+        </ModalBtn>
+        {/* 모달창 */}
+        <ModalWindow showModal={showModal}>
+          <div>
+            <button onClick={() => setShowModal(!showModal)} />
+            <p>HELLOW CODESTATES</p>
+          </div>
+        </ModalWindow>
+      </ModalBox>
+    </Box>
+  );
+};
+
+// 중앙배치
+const ModalBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+// 모달버튼
+const ModalBtn = styled.button`
+  width: 120px;
+  height: 50px;
+  border: none;
+  border-radius: 50px;
+  background: var(--main-color);
+  color: #fff;
+  cursor: pointer;
+`;
+
+// 모달창 visibility로 컨트롤
+const ModalWindow = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  visibility: ${(prop) => (prop.showModal ? "visible" : "hidden")};
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  > div {
+    height: 120px;
+    width: 300px;
+    padding: 10px;
+    border-radius: 20px;
+    background: #fff;
+  }
+  p {
+    padding-top: 25px;
+    margin: auto;
+    color: var(--main-color);
+    text-align: center;
+    font-weight: bold;
+  }
+
+  /* 모달닫기버튼 */
+  button {
+    position: relative;
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    border: none;
+    margin: 0;
+    transform: translateX(-50%);
+    background-color: transparent;
+    cursor: pointer;
+    :after,
+    :before {
+      position: absolute;
+      display: block;
+      top: 0;
+      width: 2px;
+      height: 15px;
+      background: black;
+      content: "";
+    }
+    :after {
+      left: 50%;
+      transform: rotate(45deg);
+    }
+    :before {
+      left: 50%;
+      transform: rotate(-45deg);
+    }
+  }
+`;
+
+export default Modal;
